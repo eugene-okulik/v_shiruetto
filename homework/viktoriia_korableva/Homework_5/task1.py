d3daa05ebba5060bda2ae5e9edeec3cb4291d6a8
Viktoriia Korableva <vika.burmurton@gmail.com>
2025-07-02 14:19:15 -0600

    fixed code

--- homework/viktoriia_korableva/Homework_5/task1.py
+++ homework/viktoriia_korableva/Homework_5/task1.py
@@ -5,10 +5,10 @@ print(name, last_name, city, phone, country, sep=', ')
 result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
-result_4 = 'результат работы программы: 10'  # пример для любого числа
-new_result_1 = int(result_1[result_1.index('42'):]) + 10
-new_result_2 = int(result_2[result_2.index('514'):]) + 10
-new_result_3 = int(result_3[result_3.index('9'):]) + 10
+result_4 = 'результат работы программы: 10'  # пример для лю
+new_result_1 = int(result_1.split(': ')[1]) + 10
+new_result_2 = int(result_2.split(': ')[1]) + 10
+new_result_3 = int(result_3.split(': ')[1]) + 10
 new_result_4 = int(result_4.split(': ')[1]) + 10
 print(new_result_1)
 print(new_result_2)
